8bcfff2a8708c36b90a14810234a9ac830882572
THiragi <w1th1nmybr41n@gmail.com>
2023-12-30 14:41:18 +0900

    add test route

--- src/lib/server/hono/app.ts
+++ src/lib/server/hono/app.ts
@@ -1,10 +1,11 @@
 import { Hono } from 'hono';
 import { route as hello } from './routes/hello';
+import { route as test } from './routes/test';
 
 export function createApp(basePath: string) {
 	const app = new Hono().basePath(basePath);
 
-	const route = app.route('/hello', hello);
+	const route = app.route('/hello', hello).route('/hello/rpc/test', test);
 
 	return { app, route };
 }
